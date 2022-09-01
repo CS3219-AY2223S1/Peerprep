d807739e357511582073cd29c62391592d715d01
BrendonLau <brendonlcy@hotmail.com>
2022-09-01 17:07:04 +0800

    feat(match): difficulty buttons and timer
    Update matchPage with buttons and timer upon clicking
    Update pull request template

---
+++ .github/pull_request_template.md
@@ -0,0 +1,18 @@
+## Status
+
+READY | IN DEVELOPMENT | HOLD
+
+## Description
+
+A few sentences describing the overall goals of the pull request's commits.
+
+## Screenshots
+
+A few screenshots displaying the changes made by the pull request.
+
+## Todos
+
+* [ ] Have you cleaned up your code? (Eg. remove redundant code/comments, fix warnings, refactor duplicate code etc.)
+* [ ] Have you manually tested your code extensively? (Eg. execute the happy path, try out edge cases etc.)
+* [ ] Have you linked your PR to the respective issues?
+* [ ] Have you generated documentation for your changes?
--- frontend/src/pages/MatchPage.tsx
+++ frontend/src/pages/MatchPage.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, CircularProgress } from '@mui/material';
+
 function MatchPage() {
-  return (<div>
-    <div className="flex flex-col space-y-8 w-1/3 m-auto">
-        <Button size="large" variant="contained" color="success">Easy</Button>
-        <Button size="large" variant="contained" color="warning">Medium</Button>
-        <Button size="large" variant="contained" color="error">Hard</Button>
+  const [timer, setTimer] = useState<number>(0);
+  const TIMEOUT = 10;
+  const REDIRECT_TIME = 3;
+  enum Difficulty {
+    Easy, Medium, Hard,
+  }
+
+  const startTimer = () => {
+    setTimer(TIMEOUT + REDIRECT_TIME);
+  };
+
+  useEffect(() => {
+    if (timer > 0) {
+      setTimeout(() => {
+        setTimer(timer - 1);
+      }, 1000);
+    }
+  }, [timer]);
+
+  const handleSelection = (difficulty: Difficulty) => {
+    startTimer();
+    switch (difficulty) {
+      case Difficulty.Easy:
+        // TODO
+        break;
+      case Difficulty.Medium:
+        // TODO
+        break;
+      default:
+        // TODO
+        break;
+    }
+  };
+
+  return (
+    <div className="h-1/3 w-1/3 absolute inset-0 m-auto">
+      {timer ? (
+        <div className="flex flex-col justify-center items-center">
+          <div className="mb-5 font-semibold font-sans text-3xl">{timer > REDIRECT_TIME ? 'Finding a match...' : 'Unable to find :( Please try again.'}</div>
+          {timer > REDIRECT_TIME
+            ? <div className="text-9xl animate-ping delay-1000">{timer - REDIRECT_TIME}</div> : <CircularProgress />}
+        </div>
+      ) : (
+        <div className="flex flex-col space-y-8">
+          <Button size="large" variant="contained" color="success" onClick={() => handleSelection(Difficulty.Easy)}><b>Easy</b></Button>
+          <Button size="large" variant="contained" color="warning" onClick={() => handleSelection(Difficulty.Medium)}><b>Medium</b></Button>
+          <Button size="large" variant="contained" color="error" onClick={() => handleSelection(Difficulty.Hard)}><b>Hard</b></Button>
+        </div>
+      )}
     </div>
-  </div>);
+  );
 }
 
 export default MatchPage;
