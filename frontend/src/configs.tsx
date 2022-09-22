5c53dfc33504a9f02b9e86b0596ca79ee55aca48
justinyjt <e0560296@u.nus.edu>
2022-09-22 17:38:18 +0800

    feat(user): Beautify delete FE and complete change password FE/BE

--- frontend/src/configs.tsx
+++ frontend/src/configs.tsx
@@ -7,3 +7,5 @@ export const URL_USER_SIGNUP_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/signup`;
 export const URL_USER_LOGIN_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/login`;
 
 export const URL_USER_DELETE_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/delete`;
+
+export const URL_USER_CHANGE_PW_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/change_password`;
