dbcb327dd9045756eee28af0259becb14a46ff0b
pangyuanker <e0660595@u.nus.edu>
2022-10-11 19:55:45 +0800

    Update question modal to make calls to backend api

--- frontend/src/components/room/QuestionModal.tsx
+++ frontend/src/components/room/QuestionModal.tsx
@@ -1,37 +1,72 @@
-import React, { useState } from 'react';
-import {
-  Box, Button, Modal, Typography,
-} from '@mui/material';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { useEffect, useState } from "react";
+import { Box, Button, Modal, Typography } from "@mui/material";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import axios from "axios";
+import { URL_QUESTION_GET_RANDOM } from "../../configs";
+import { STATUS_CODE_SUCCESS } from "../../constants";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
-const dummyHeader = '2 Sum';
-const dummyBody = 'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.';
+
+const dummyDifficulty = "Easy peasy";
 
 const QuestionModal = () => {
   const [open, setOpen] = useState(false);
-  const [copied, setCopied] = useState(false);
-  const [body, setBody] = useState(dummyBody);
+  const [printBody, setPrintBody] = useState("Uh oh... No questions yet");
+  const [printTitle, setPrintTitle] = useState("No title");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [render, setRender] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const handleCopy = () => {
-    setCopied(true);
-    setOpen(false);
+  useEffect(() => {
+    setPrintTitle(title);
+    setPrintBody(body);
+    setRender(false);
+  }, [render]);
+
+  const handleOpenQuestionModal = async () => {
+    handleOpen();
+    await getQuestion();
+    setRender(true);
   };
+
+  const handleNextQuestion = async () => {
+    await getQuestion();
+    setRender(true);
+  };
+
+  const getQuestion = async () => {
+    const res = await axios
+      .get(URL_QUESTION_GET_RANDOM, {
+        headers: { difficulty: dummyDifficulty },
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    if (res && res.status === STATUS_CODE_SUCCESS) {
+      setTitle(res.data.message.title);
+      setBody(res.data.message.content);
+    }
+  };
+
   return (
     <div>
-      <Button variant="contained" size="small" onClick={handleOpen}>
+      <Button
+        variant="contained"
+        size="small"
+        onClick={handleOpenQuestionModal}
+      >
         Question
       </Button>
       <Modal
@@ -42,19 +77,23 @@ const QuestionModal = () => {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            {dummyHeader}
+            {printTitle}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2, mb: 2 }}>
-            {dummyBody}
+            {printBody}
           </Typography>
           <Box className="float-right flex grow-0 flex-row justify-items-end w-100">
-            <CopyToClipboard text={body} onCopy={handleCopy}>
+            <CopyToClipboard text={body} onCopy={handleClose}>
               <Button variant="contained" size="small">
                 Copy
               </Button>
             </CopyToClipboard>
             <div className="mr-1 ml-1" />
-            <Button variant="contained" size="small">
+            <Button
+              variant="contained"
+              size="small"
+              onClick={handleNextQuestion}
+            >
               Next
             </Button>
           </Box>
--- frontend/src/configs.tsx
+++ frontend/src/configs.tsx
@@ -1,7 +1,11 @@
-const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000';
-const URI_SOCKET_SVC = process.env.URI_SOCKET_SVC || 'http://localhost:8001';
+const URI_USER_SVC = process.env.URI_USER_SVC || "http://localhost:8000";
+const URI_SOCKET_SVC = process.env.URI_SOCKET_SVC || "http://localhost:8001";
+const URI_QUESTION_SVC =
+  process.env.URI_QUESTION_SVC || "http://localhost:8003";
 
-const PREFIX_USER_SVC = '/api/user';
+const PREFIX_USER_SVC = "/api/user";
+
+const PREFIX_QUESTION_SVC = "/questions/test";
 
 export const URL_USER_SIGNUP_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/signup`;
 
@@ -9,6 +13,12 @@ export const URL_USER_LOGIN_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/login`;
 
 export const URL_USER_DELETE_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/delete`;
 
-export const URL_USER_CHANGE_PW_SVC = `${URI_USER_SVC + PREFIX_USER_SVC}/change_password`;
+export const URL_USER_CHANGE_PW_SVC = `${
+  URI_USER_SVC + PREFIX_USER_SVC
+}/change_password`;
+
+export const URL_QUESTION_GET_RANDOM = `${
+  URI_QUESTION_SVC + PREFIX_QUESTION_SVC
+}/getOne`;
 
 export const URL_GET_ROOM_UUID = `${URI_SOCKET_SVC}/room/roomUuid`;
