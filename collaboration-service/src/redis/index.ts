feab543707a91b672deba680fa7b3899bc4f3f8a
elton <eltongjh@u.nus.edu>
2022-11-02 12:34:33 +0800

    feat(docker): Dockerize microservices except for frontend

--- collaboration-service/src/redis/index.ts
+++ collaboration-service/src/redis/index.ts
@@ -1,5 +1,10 @@
 import { createClient } from 'redis';
 
-const redisClient = createClient();
+const redisClient = createClient({
+  socket: {
+    host: process.env.REDIS || 'localhost',
+    port: 6379,
+  },
+});
 
 export default redisClient;
